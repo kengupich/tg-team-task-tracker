fca129249b173364da1c5de470e2bb91c53c0931
kengupich <kengups@gmail.com>
2026-05-24 21:53:19 +0300

    test5

--- handlers/tasks/creation.py
+++ handlers/tasks/creation.py
@@ -74,8 +74,8 @@ async def create_task(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int
         return ConversationHandler.END """
     
     # Get user's group (if they have one)
-    """ user_group_id = None
-    if is_group_admin(user_id):
+    user_group_id = 1
+    """ if is_group_admin(user_id):
         user_group_id = get_user_group_id(user_id)
     else:
         user = get_user_by_id(user_id)
